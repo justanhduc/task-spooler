800914962be2e49addf757679ea04f67f7edb2a8
kylin <kylincaster@foxmail.com>
2023-03-27 17:46:27 +0800

    add default_path.h

---
+++ default_path.h
@@ -0,0 +1,3 @@
+#define DEFAULT_USER_PATH "/home/kylin/task-spooler/user.txt"
+#define DEFAULT_LOG_PATH "/home/kylin/task-spooler/log.txt"
+#define DEFAULT_SQLITE_PATH "/home/kylin/task-spooler/task-spooler.db"
